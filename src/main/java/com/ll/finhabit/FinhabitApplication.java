68cae5f808fd482cd686daa650f178a674fab8e1
naeun <1210naeun@gmail.com>
2025-11-09 01:05:34 +0900

    ⚙️  setting : gitignore 수정

---
+++ src/main/java/com/ll/finhabit/FinhabitApplication.java
@@ -0,0 +1,13 @@
+package com.ll.finhabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinhabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinhabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/finhabit/FinhabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.finhabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinhabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
