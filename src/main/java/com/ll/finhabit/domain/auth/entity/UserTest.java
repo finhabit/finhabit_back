200fc5f27de72a262228ad938c2983a5d80d5d97
naeun <1210naeun@gmail.com>
2025-11-30 14:21:11 +0900

    🚀  Deploy test

---
+++ src/main/java/com/ll/finhabit/domain/auth/entity/UserTest.java
@@ -0,0 +1,29 @@
+package com.ll.finhabit.domain.auth.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserTest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
