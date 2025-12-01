c9ee68d95ee42ba08f03acf4f3253b13824a1205
naeun <1210naeun@gmail.com>
2025-12-01 11:41:33 +0900

    🔧  fix : Security Config

--- src/main/java/com/ll/finhabit/config/SecurityConfig.java
+++ src/main/java/com/ll/finhabit/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.ll.finhabit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +22,23 @@ public class SecurityConfig {
                         .allowCredentials(true);
             }
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/api-docs"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+        // 선택적으로 .csrf().disable() 등 추가
+        ;
+
+        return http.build();
     }
 }
